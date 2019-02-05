f71e8f6bbde65dd69c8d5f6f162821df7cb41099
Katie Moosman <moosman.katie@gmail.com>
2019-02-05 08:16:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,6 +34,17 @@ class Student
     end
   end
 
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+      SELECT * FROM students
+      LIMIT ? 
+    SQL
+
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def self.all
     # retrieve all the rows from the "Students" database
     # find the student in the database given a name
