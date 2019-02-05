f615d6ab75fd3910654b671792773f5e84d11c02
Katie Moosman <moosman.katie@gmail.com>
2019-02-05 08:16:15 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,7 +37,7 @@ class Student
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
       SELECT * FROM students
-      LIMIT ? 
+      LIMIT ?
     SQL
 
     DB[:conn].execute(sql, x).map do |row|
